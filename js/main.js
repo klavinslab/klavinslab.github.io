c7b2be222c1c87a06dba1182c4baae49a3cea9a5
Eric Klavins <klavins@uw.edu>
2014-09-22 19:13:37 -0700

    tech reports

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 
+
 window.onload=function() {
   
   var elems = document.getElementsByClassName('paper');
+  var n = elems.length;
+  for (i in elems) {
+    elems[i].innerHTML = "<span class='num'>[" + n + "]</span> " + elems[i].innerHTML;
+    n--;
+  }
+
+  var elems = document.getElementsByClassName('report');
+  var n = elems.length;
+  for (i in elems) {
+    elems[i].innerHTML = "<span class='num'>[" + n + "]</span> " + elems[i].innerHTML;
+    n--;
+  }
+
+  var elems = document.getElementsByClassName('thesis');
   var n = elems.length;
   for (i in elems) {
     elems[i].innerHTML = "<span class='num'>[" + n + "]</span> " + elems[i].innerHTML;
